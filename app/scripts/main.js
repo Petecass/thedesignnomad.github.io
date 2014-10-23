281f5882cffe79b8e84dcbae4c2f0061c1928646
Mod Kaffes <modkaffes@gmail.com>
2014-10-23 03:08:13 +0300

    Slightly update work section. Add bio section. Add bucketlist section. Add contact section.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,9 @@
-$('.header-wrapper').height($(window).height()-100);
+// Make .header-wrapper height equal to the viewport height
+$('.js-header-wrapper').height($(window).height()-100);
+
+
+// Make .contact-wrapper push inside .bucketlist-wrapper
+var $contactWrapperHeight = $('.js-contact-wrapper').outerHeight();
+$('.js-contact-wrapper').css('margin-top', -$contactWrapperHeight);
+var $jsBucketlistWrapperPaddingBottom = parseInt($('.js-bucketlist-wrapper').css('padding-bottom').replace(/\D/g, ''));
+$('.js-bucketlist-wrapper').css('padding-bottom', $jsBucketlistWrapperPaddingBottom + $contactWrapperHeight);
